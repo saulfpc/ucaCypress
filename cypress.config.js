9c891e7bc065f9aaedcf4a02fb264eb80c9cfbb7
saulfpc <psaulcar@gmail.com>
2023-06-12 08:45:18 -0600

    Code Challenge Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  "chromeWebSecurity": false,
+  "blockHosts":[
+    "www.saucedemo.com/__socket/?EIO=4&transport=websocket",
+    "www.saucedemo.com/__socket-graphql"
+  ],
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/code challenge/code.cy.js
+++ cypress/e2e/code challenge/code.cy.js
@@ -1,8 +1,26 @@
-it.skip('code challenge', () => {
-    cy.visit("https://www.google.com/")
-  })
+/**
+ * el problema durante el taller se  debio a un tema de BE
+ * donde al no completarse dos request el sitio web
+ * no hacia el trigger del evento load
+ * lo que le decia a cypress que el sitio web ya estaba listo
+ * solucionamos este problema
+ * llendo a cypress.config.js
+ * y agrengando las siguientes configuraciones en la linea 4
+ *  "blockHosts":[
+    "www.saucedemo.com/__socket/?EIO=4&transport=websocket",
+    "www.saucedemo.com/__socket-graphql"
+  ],
+ */
+
+  /**
+   * removemos el should('be.clickable') ya que ese assert
+   * no existe y su equivalente de enabled
+   * no funciona bien con el boton
+   * y actualizaremos
+   * should("have.value") por should("have.length")
+   */
 it('code challenge', () => {
-    cy.visit("https://www.saucedemo.com/")
+    cy.visit({url:"https://www.saucedemo.com/",method: 'GET'})
     cy.get('[data-test="username"]')
     .type(`standard_user`)
     cy.get('#password')
@@ -14,10 +32,9 @@ it('code challenge', () => {
     cy.get("button.btn_inventory").eq(0)
     .should('have.text','Remove')
     cy.get("button.btn_inventory").eq(1).click()
-    cy.get("a.shopping_cart_link").should("be.clickable")
-    .click()
-    cy.get("div.cart_item").should("have.value",2)
+    cy.get("a.shopping_cart_link").click()
+    cy.get("div.cart_item").should("have.length",2)
     cy.contains("Remove").click()
-    cy.get("div.cart_item").should("have.value",1)
+    cy.get("div.cart_item").should("have.length",1)
     cy.contains("Checkout").click()
   })
